34126f225b97735eeaf1dd767adec9936a5b57ba
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-06-11 13:44:17 +0200

    Include docs folder

---
+++ R/fyrtaarn-package.R
@@ -0,0 +1,4 @@
+#' @details
+#' Analysis of data on injuries from NPR and KPR data
+#' @keywords internal
+"_PACKAGE"
--- dev/setup.R
+++ dev/setup.R
@@ -1,12 +1,17 @@
 # usethis::create_package("fyrtaarn")
 usethis::use_r("case.R")
 usethis::use_r("utils.R")
+usethis::use_package("knitr", "Suggests")
+
 
 devtools::load_all()
 roxygen2::roxygenise(clean = TRUE)
 devtools::document()
 devtools::check()
 
+
+# Run once to configure your package to use pkgdown
+usethis::use_pkgdown()
 
 
 pkgdown::build_site()
