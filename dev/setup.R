ac5eaca2ba22ca9400dee199a014b22ef25657cf
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-07-19 13:07:23 +0200

    Misc: Restructure

--- dev/setup.R
+++ dev/setup.R
@@ -1,8 +1,5 @@
-# usethis::create_package("fyrtaarn")
-usethis::use_r("case.R")
-usethis::use_r("utils.R")
-usethis::use_package("knitr", "Suggests")
 
+# usethis::create_package("fyrtaarn")
 
 devtools::load_all()
 roxygen2::roxygenise(clean = TRUE)
@@ -18,6 +15,9 @@ pkgdown::build_site()
 pkgdown::preview_site()
 pkgdown::build_news(preview = TRUE)
 
+usethis::use_r("case.R")
+usethis::use_r("utils.R")
+usethis::use_package("knitr", "Suggests")
 usethis::use_package("data.table", min_version = TRUE)
 
 usethis::use_build_ignore("dev")
