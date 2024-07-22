417dfb225ad873f4235eb271fd288180ec278f8d
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2024-07-22 20:15:14 +0200

    Feat: Encode non-ASCII characters

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,4 @@
+
 ## delidx <- dt[colName %in% selecedValues, which = TRUE]
 ## Ref https://github.com/Rdatatable/data.table/issues/635#issuecomment-261473829
 is_delete_index <- function(dt, delidx){
@@ -14,3 +15,13 @@ is_delete_index <- function(dt, delidx){
 
   return(dtSub)
 }
+
+
+# Encoding solution with some modification from
+# https://github.com/StoXProject/RstoxData/issues/10#issuecomment-510542301
+is_encode <- function(x) gsub("\\u00c3\\u00a6|\xe6", "\u00e6", useBytes = TRUE,
+                              gsub("\\u00c3\\u00a5|\xe5", "\u00e5", useBytes = TRUE,
+                                   gsub("\\u00c3\\u00b8|\xf8", "\u00f8", useBytes = TRUE,
+                                        gsub("\xed", "i", useBytes = TRUE,
+                                             gsub("\xc5", "\u00c5", useBytes = TRUE,
+                                                  gsub("\xd8", "\u00d8", x, useBytes = TRUE))))))
